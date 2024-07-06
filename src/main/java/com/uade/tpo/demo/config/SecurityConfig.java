58a5fe9183400bc82062c1cec02370246ab2fa81
serleo <serleo96.98@gmail.com>
2024-07-05 21:41:32 -0300

    chau cors

--- src/main/java/com/uade/tpo/demo/config/SecurityConfig.java
+++ src/main/java/com/uade/tpo/demo/config/SecurityConfig.java
@@ -24,7 +24,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.httpBasic().disable().csrf(AbstractHttpConfigurer::disable)
+        http.httpBasic().disable()
+                .cors().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req -> req
 
